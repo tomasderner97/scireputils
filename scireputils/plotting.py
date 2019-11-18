ecccd3369b63110f114b9060877f5cc7ec728a52
Tomáš Derner <tomasderner97@gmail.com>
2019-11-18 18:14:46 +0100

    add use latex style

--- scireputils/plotting.py
+++ scireputils/plotting.py
@@ -14,3 +14,7 @@ def latex_style():
         plt...
     """
     return plt.style.context(_MATPLOTLIB_LATEX_STYLE_PATH)
+
+
+def matplotlib_use_latex_style():
+    plt.style.use(_MATPLOTLIB_LATEX_STYLE_PATH)
