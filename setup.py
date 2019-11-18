1fdae9f0d750b59b73f78126537fe56a38ab65e8
Tomáš Derner <tomasderner97@gmail.com>
2019-11-18 17:21:33 +0100

    edited packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.1',
     url='https://github.com/tomasderner97/scireputils.git',
     author='Tomáš Derner',
-    packages=find_packages(),
+    packages=['scireputils'],
     install_requires=[
         'numpy',
         'matplotlib',
