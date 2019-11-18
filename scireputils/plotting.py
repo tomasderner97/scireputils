db6b831384da5db26c58e6dea4e41ebd6e5ad4e6
Tomáš Derner <tomasderner97@gmail.com>
2019-11-18 17:04:20 +0100

    added matplotlib latex style

---
+++ scireputils/plotting.py
@@ -0,0 +1,16 @@
+import os
+
+import matplotlib.pyplot as plt
+
+_MATPLOTLIB_LATEX_STYLE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "latex_style.mplstyle")
+
+
+def latex_style():
+    """
+    Used as context.
+    Usage
+    -----
+    with latex_style():
+        plt...
+    """
+    return plt.style.context(_MATPLOTLIB_LATEX_STYLE_PATH)
