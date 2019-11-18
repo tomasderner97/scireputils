8523b84ea84feefca1ac8919775e5983c85d3de2
Tomáš Derner <tomasderner97@gmail.com>
2019-11-18 18:18:10 +0100

    made changes about latex style

--- scireputils/plotting.py
+++ scireputils/plotting.py
@@ -1,20 +1,15 @@
 import os
 
+import matplotlib
+
 import matplotlib.pyplot as plt
 
 _MATPLOTLIB_LATEX_STYLE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "latex_style.mplstyle")
 
 
-def latex_style():
-    """
-    Used as context.
-    Usage
-    -----
-    with latex_style():
-        plt...
-    """
-    return plt.style.context(_MATPLOTLIB_LATEX_STYLE_PATH)
-
-
 def matplotlib_use_latex_style():
     plt.style.use(_MATPLOTLIB_LATEX_STYLE_PATH)
+
+
+def matplotlib_restore_default_style():
+    matplotlib.rcParams.update(matplotlib.rcParamsDefault)
