6f7224063d9918b57da9ddf321f661bd078627b3
Tomáš Derner <tomasderner97@gmail.com>
2019-11-19 18:56:49 +0100

    fix

--- scireputils/latex_templates.py
+++ scireputils/latex_templates.py
@@ -2,23 +2,25 @@ import os
 
 import jinja2
 
-latex_jinja_env = jinja2.Environment(
-    block_start_string=r'\BLOCK{',
-    block_end_string='}',
-    variable_start_string=r'\VAR{',
-    variable_end_string='}',
-    comment_start_string=r'\#{',
-    comment_end_string='}',
-    line_statement_prefix='%%',
-    line_comment_prefix='%#',
-    trim_blocks=True,
-    autoescape=False,
-    loader=jinja2.FileSystemLoader(os.path.abspath('.'))
-)
-
 
 def render_template(template_path: str, output_path: str, **variables):
-    template = latex_jinja_env.get_template(template_path)
+    template_dir, template_name = os.path.split(template_path)
+
+    latex_jinja_env = jinja2.Environment(
+        block_start_string=r'\BLOCK{',
+        block_end_string='}',
+        variable_start_string=r'\VAR{',
+        variable_end_string='}',
+        comment_start_string=r'\#{',
+        comment_end_string='}',
+        line_statement_prefix='%%',
+        line_comment_prefix='%#',
+        trim_blocks=True,
+        autoescape=False,
+        loader=jinja2.FileSystemLoader(os.path.abspath(template_dir))
+    )
+
+    template = latex_jinja_env.get_template(template_name)
     rendered = template.render(section1='Long Form', section2='Short Form')
 
     with open(output_path, "w+") as out:
