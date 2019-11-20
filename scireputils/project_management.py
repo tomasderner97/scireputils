44b03635d9db9a86fd7788a7fc63b3a4de271495
Tomáš Derner <tomasderner97@gmail.com>
2019-11-20 22:31:35 +0100

    changed the init function

--- scireputils/project_management.py
+++ scireputils/project_management.py
@@ -1,8 +1,23 @@
+import glob
 import os
 import shutil
 
 _FILE_DIR = os.path.split(__file__)[0]
 _TEMPLATE_PATH = os.path.join(_FILE_DIR, "report_template")
+
+_TEMPLATE_STRUCTURE = {
+    "": ["makefile"],
+    "auxiliary": [],
+    "classfiles": glob.glob(os.path.join(_TEMPLATE_PATH, "*.cls")),
+    "data": [],
+    "figures": [],
+    "latex": [],
+    "output": [],
+    "plots": [],
+    "raw_data": [],
+    "scripts": glob.glob(os.path.join(_TEMPLATE_PATH, "*.py")),
+    "templates": glob.glob(os.path.join(_TEMPLATE_PATH, "*.tex")),
+}
 
 
 def ensure_directory_exists(path):
@@ -14,32 +29,9 @@ def init_report_directory(path=""):
     if not path:
         path = os.getcwd()
 
-    ensure_directory_exists(path)
+    for directory, files in _TEMPLATE_STRUCTURE.items():
+        dir_path = os.path.join(path, directory)
+        ensure_directory_exists(dir_path)
 
-    auxiliary_path = os.path.join(path, "auxiliary")
-    classfiles_path = os.path.join(path, "classfiles")
-    data_path = os.path.join(path, "data")
-    figures_path = os.path.join(path, "figures")
-    latex_path = os.path.join(path, "latex")
-    output_path = os.path.join(path, "output")
-    plots_path = os.path.join(path, "plots")
-    raw_data_path = os.path.join(path, "raw_data")
-    scripts_path = os.path.join(path, "scripts")
-    templates_path = os.path.join(path, "templates")
-
-    ensure_directory_exists(auxiliary_path)
-    ensure_directory_exists(classfiles_path)
-    ensure_directory_exists(data_path)
-    ensure_directory_exists(figures_path)
-    ensure_directory_exists(latex_path)
-    ensure_directory_exists(output_path)
-    ensure_directory_exists(plots_path)
-    ensure_directory_exists(raw_data_path)
-    ensure_directory_exists(scripts_path)
-    ensure_directory_exists(templates_path)
-
-    shutil.copy(os.path.join(_TEMPLATE_PATH, "makefile"), path)
-    shutil.copy(os.path.join(_TEMPLATE_PATH, "project_wide.py"), scripts_path)
-    shutil.copy(os.path.join(_TEMPLATE_PATH, "render.py"), scripts_path)
-    shutil.copy(os.path.join(_TEMPLATE_PATH, "main.tex"), templates_path)
-    shutil.copy(os.path.join(_TEMPLATE_PATH, "scirep.cls"), classfiles_path)
+        for file in files:
+            shutil.copy(os.path.join(_TEMPLATE_PATH, file), dir_path)
