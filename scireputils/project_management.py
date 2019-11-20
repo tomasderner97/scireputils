f24aac866e4c5ed3bb54179658bd247bebab1e2c
Tomáš Derner <tomasderner97@gmail.com>
2019-11-20 22:06:41 +0100

    changed the init function

--- scireputils/project_management.py
+++ scireputils/project_management.py
@@ -1,7 +1,8 @@
 import os
-from distutils.dir_util import copy_tree
+import shutil
 
-_TEMPLATE_PATH = os.path.join(os.path.split(__file__)[0], "report_template")
+_FILE_DIR = os.path.split(__file__)[0]
+_TEMPLATE_PATH = os.path.join(_FILE_DIR, "report_template")
 
 
 def ensure_directory_exists(path):
@@ -15,4 +16,30 @@ def init_report_directory(path=""):
 
     ensure_directory_exists(path)
 
-    copy_tree(_TEMPLATE_PATH, path)
+    auxiliary_path = os.path.join(path, "auxiliary")
+    classfiles_path = os.path.join(path, "classfiles")
+    data_path = os.path.join(path, "data")
+    figures_path = os.path.join(path, "figures")
+    latex_path = os.path.join(path, "latex")
+    output_path = os.path.join(path, "output")
+    plots_path = os.path.join(path, "plots")
+    raw_data_path = os.path.join(path, "raw_data")
+    scripts_path = os.path.join(path, "scripts")
+    templates_path = os.path.join(path, "templates")
+
+    os.mkdir(auxiliary_path)
+    os.mkdir(classfiles_path)
+    os.mkdir(data_path)
+    os.mkdir(figures_path)
+    os.mkdir(latex_path)
+    os.mkdir(output_path)
+    os.mkdir(plots_path)
+    os.mkdir(raw_data_path)
+    os.mkdir(scripts_path)
+    os.mkdir(templates_path)
+
+    shutil.copy(os.path.join(_TEMPLATE_PATH, "makefile"), path)
+    shutil.copy(os.path.join(_TEMPLATE_PATH, "project_wide.py"), scripts_path)
+    shutil.copy(os.path.join(_TEMPLATE_PATH, "render.py"), scripts_path)
+    shutil.copy(os.path.join(_TEMPLATE_PATH, "main.tex"), templates_path)
+    shutil.copy(os.path.join(_TEMPLATE_PATH, "scirep.cls"), classfiles_path)
