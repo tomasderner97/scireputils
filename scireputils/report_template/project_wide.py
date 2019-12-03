503433b01896b6ccac46a2b668bab768eb339f49
Tomáš Derner <tomasderner97@gmail.com>
2019-12-03 17:49:03 +0100

    decision made about project generation

---
+++ scireputils/report_template/project_wide.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+AUXILIARY_PATH = "../auxiliary"
+CLASSFILES_PATH = "../classfiles"
+DATA_PATH = "../data"
+FIGURES_PATH = "../figures"
+LATEX_PATH = "../latex"
+OUTPUT_PATH = "../output"
+PLOTS_PATH = "../plots"
+RAW_DATA_PATH = "../raw_data"
+SCRIPTS_PATH = "../scripts"
+TEMPLATES_PATH = "../templates"
