eb3a8f8cb0a6ae675e0d8c5ed8f0db686db89758
Tomáš Derner <tomasderner97@gmail.com>
2019-12-04 13:58:22 +0100

    dunno

--- scireputils/report_template/project_wide.py
+++ scireputils/report_template/project_wide.py
@@ -1,13 +1,15 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-AUXILIARY_PATH = "../auxiliary"
-CLASSFILES_PATH = "../classfiles"
-DATA_PATH = "../data"
-FIGURES_PATH = "../figures"
-LATEX_PATH = "../latex"
-OUTPUT_PATH = "../output"
-PLOTS_PATH = "../plots"
-RAW_DATA_PATH = "../raw_data"
-SCRIPTS_PATH = "../scripts"
-TEMPLATES_PATH = "../templates"
+AUXILIARY_PATH = os.path.join("..", "auxiliary")
+CLASSFILES_PATH = os.path.join("..", "classfiles")
+DATA_PATH = os.path.join("..", "data")
+FIGURES_PATH = os.path.join("..", "figures")
+LATEX_PATH = os.path.join("..", "latex")
+OUTPUT_PATH = os.path.join("..", "output")
+PLOTS_PATH = os.path.join("..", "plots")
+RAW_DATA_PATH = os.path.join("..", "raw_data")
+SCRIPTS_PATH = os.path.join("..", "scripts")
+TEMPLATES_PATH = os.path.join("..", "templates")
--- scireputils/report_template/render.py
+++ scireputils/report_template/render.py
@@ -1,6 +1,18 @@
-from scireputils.latex_templates import render_template
+import os
 
+from project_wide import TEMPLATES_PATH, LATEX_PATH, OUTPUT_PATH
+from scireputils.latex_templates import render_template, compile_latex_to_pdf
 
-render_template("../templates/main.tex",
-                "../latex/main.tex",
+template_params = {
+
+}
+
+TEMPLATE_PATH = os.path.join(TEMPLATES_PATH, "main.tex")
+LATEX_OUT_PATH = os.path.join(LATEX_PATH, "main.tex")
+PDF_PATH = os.path.join(OUTPUT_PATH, "main.pdf")
+
+render_template(TEMPLATE_PATH,
+                LATEX_OUT_PATH,
                 )
+compile_latex_to_pdf(LATEX_OUT_PATH,
+                     PDF_PATH)
